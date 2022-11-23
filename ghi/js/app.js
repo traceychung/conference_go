4a26fb30788039474bb5ecf0dc3d831b838b33bc
tracey chung <tchung97@gmail.com>
2022-11-22 16:39:15 -0800

    add date

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,47 +1,51 @@
-function createCard(name, description, pictureUrl) {
+function createCard(name, description, pictureUrl, start, end) {
     return `
         <div class="col">
-            <div class="card mb-4">
+            <div class="card mb-4 shadow">
                 <img src="${pictureUrl}" class="card-img-top">
                 <div class="card-body">
                 <h5 class="card-title">${name}</h5>
                 <p class="card-text">${description}</p>
                 </div>
+                <div class="card-footer">
+                <small class="text-muted">${start}-${end}</small>
+              </div>
             </div>
         </div>
     `;
   }
 
+window.addEventListener('DOMContentLoaded', async () => {
 
-  window.addEventListener('DOMContentLoaded', async () => {
+const url = 'http://localhost:8000/api/conferences/';
 
-    const url = 'http://localhost:8000/api/conferences/';
+try {
+    const response = await fetch(url);
 
-    try {
-      const response = await fetch(url);
+    if (!response.ok) {
+    // Figure out what to do when the response is bad
+    } else {
+    const data = await response.json();
 
-      if (!response.ok) {
-        // Figure out what to do when the response is bad
-      } else {
-        const data = await response.json();
-
-        for (let conference of data.conferences) {
-          const detailUrl = `http://localhost:8000${conference.href}`;
-          const detailResponse = await fetch(detailUrl);
-          if (detailResponse.ok) {
-            const details = await detailResponse.json();
-            const title = details.conference.name;
-            const description = details.conference.description;
-            const pictureUrl = details.conference.location.picture_url;
-            const html = createCard(title, description, pictureUrl);
-            const column = document.querySelector('.col');
-            column.innerHTML += html;
-          }
+    for (let conference of data.conferences) {
+        const detailUrl = `http://localhost:8000${conference.href}`;
+        const detailResponse = await fetch(detailUrl);
+        if (detailResponse.ok) {
+        const details = await detailResponse.json();
+        const title = details.conference.name;
+        const description = details.conference.description;
+        const pictureUrl = details.conference.location.picture_url;
+        const start = new Date(details.conference.ends).toLocaleDateString();
+        const end = new Date(details.conference.starts).toLocaleDateString();
+        const html = createCard(title, description, pictureUrl, start, end);
+        const column = document.querySelector('.row-cols-1');
+        column.innerHTML += html;
         }
-
-      }
-    } catch (e) {
-      // Figure out what to do if an error is raised
     }
 
-  });
+    }
+} catch (e) {
+    // Figure out what to do if an error is raised
+}
+
+});
